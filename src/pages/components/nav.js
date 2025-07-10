a1873c016b475d8afe6202eabe90e619967aba9e
Gabriel Arteaga <gabriel.arteaga@thirtymadison.com>
2025-07-10 14:02:03 -0700

    feat: toggle eng toolkit on click

--- src/pages/components/nav.js
+++ src/pages/components/nav.js
@@ -1,11 +1,11 @@
 import {
-  faBookBookmark,
-  faHeartPulse
+  faHeartPulse,
+  faToolbox
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 
-const Navbar = () => {
+const Navbar = ({ onEngToolkitToolbarToggle, isEngToolkitToolbarEnabled }) => {
   return (
     <div className='px-3 w-full z-[100] fixed top-0 bg-black/80 backdrop-blur-sm h-[55px] justify-between items-center align-middle flex select-none cursor-default'>
       <Link href={'/'}>
@@ -18,17 +18,21 @@ const Navbar = () => {
         </p>
       </Link>
 
-        <a
-          href='https://www.notion.so/thirtymadison/Eng-Toolkit-d838300886244d9385d08e46b41b51f9#4e39228e65024b0eb31ce3a5f65367f2'
+        <button
           className='p-1 px-3 flex h-min items-center font-bold text-white rounded-md bg-gradient-to-r from-zinc-600 to-zinc-700 hover:bg-blue-300'
-          target='_blank'
+          onClick={() => {
+            if (!isEngToolkitToolbarEnabled) {
+              window.scrollTo({ top: 0, behavior: 'smooth' });
+            }
+
+            onEngToolkitToolbarToggle(!isEngToolkitToolbarEnabled)}}
         >
           <FontAwesomeIcon
-            icon={faBookBookmark}
+            icon={faToolbox}
             className='pr-2 hidden md:inline-block'
           />
           Eng Toolkit
-        </a>
+        </button>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,25 @@
+import { useState } from 'react';
+
 import AppCard from './components/appCard';
 import EngToolkitToolbar from './components/engToolkitToolbar';
 import Navbar from './components/nav';
 
 export default function Home() {
+  const [isEngToolkitToolbarEnabled, setIsEngToolkitToolbarEnabled] = useState(false);
+
   return (
     <>
       <div className='min-h-screen bg-zinc-900 pt-14'>
-        <Navbar />
+        <Navbar onEngToolkitToolbarToggle={setIsEngToolkitToolbarEnabled} isEngToolkitToolbarEnabled={isEngToolkitToolbarEnabled} />
 
         <div className='flex flex-col items-center justify-center align-middle px-4'>
           <div className='max-w-7xl w-full'>
-            <EngToolkitToolbar />
-            <AppCard />
+            {isEngToolkitToolbarEnabled && <EngToolkitToolbar />}
+
+
+            <div className='mt-6'>
+              <AppCard />
+            </div>
           </div>
         </div>
       </div>
