b4ce26c33fde13a3b09828e0a859b6b9bd11149c
Gabriel Arteaga <gabriel.arteaga@thirtymadison.com>
2025-07-10 09:53:45 -0700

    remove auth endpoint, components, and page

--- src/pages/components/nav.js
+++ src/pages/components/nav.js
@@ -1,72 +1,34 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBookBookmark,
-  faHeartBroken,
-  faHeartPulse,
-  faProcedures,
-  faWaveSquare,
+  faHeartPulse
 } from '@fortawesome/free-solid-svg-icons';
-import { faDochub, faGithub } from '@fortawesome/free-brands-svg-icons';
-import AuthButton from './authButton';
-// import AuthButton from './authButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
 
 const Navbar = () => {
-  const { data: session } = useSession();
   return (
     <div className='px-3 w-full z-[100] fixed top-0 bg-black/80 backdrop-blur-sm h-[55px] justify-between items-center align-middle flex select-none cursor-default'>
-      {' '}
       <Link href={'/'}>
         <p className='font-bold text-gray-100 text-xl'>
-          {' '}
           <FontAwesomeIcon
             icon={faHeartPulse}
             className='pr-2 hidden md:inline-block'
-          />{' '}
+          />
           PM2 UI
-        </p>{' '}
+        </p>
       </Link>
-      <div className={{ session } ? 'flex gap-2' : 'hidden'}>
-        <button
-          onClick={() =>
-            window.open(
-              'https://pm2.keymetrics.io/docs/usage/quick-start/',
-              '_blank'
-            )
-          }
+
+        <a
+          href='https://www.notion.so/thirtymadison/Eng-Toolkit-d838300886244d9385d08e46b41b51f9#4e39228e65024b0eb31ce3a5f65367f2'
           className='p-1 px-3 flex h-min items-center font-bold text-white rounded-md bg-gradient-to-r from-zinc-600 to-zinc-700 hover:bg-blue-300'
+          target='_blank'
         >
-          {' '}
           <FontAwesomeIcon
             icon={faBookBookmark}
             className='pr-2 hidden md:inline-block'
-          />{' '}
-          PM2
-        </button>
-        <button
-          onClick={() => window.open('https://www.dev.azure.com', '_blank')}
-          className='p-1 px-3 flex h-min items-center font-bold text-white rounded-md bg-gradient-to-r from-zinc-600 to-zinc-700 hover:bg-blue-300'
-        >
-          <FontAwesomeIcon
-            icon={faWaveSquare}
-            className='pr-2 hidden md:inline-block'
-          />{' '}
-          AZURE
-        </button>
-        <button
-          onClick={() =>
-            window.open('https://github.com/thenickygee/pm2-ui', '_blank')
-          }
-          className='p-1 px-3 flex h-min items-center font-bold text-white rounded-md bg-gradient-to-r from-zinc-600 to-zinc-700   hover:bg-blue-300'
-        >
-          <FontAwesomeIcon icon={faGithub} className='pr-2 hidden md:flex' />{' '}
-          GITHUB
-        </button>{' '}
-        <AuthButton />
-      </div>
+          />
+          Eng Toolkit
+        </a>
     </div>
   );
 };
